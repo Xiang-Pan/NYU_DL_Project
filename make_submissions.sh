9aa71c9e96d4e1a7d3cf20e4c2ea7dc095021b79
XiangPan <xiangpan@nyu.edu>
2022-04-25 22:27:41 -0400

    dl_sub2

--- make_submissions.sh
+++ make_submissions.sh
@@ -2,7 +2,7 @@
  # @Created by: Xiang Pan
  # @Date: 2022-04-25 21:51:43
  # @LastEditors: Xiang Pan
- # @LastEditTime: 2022-04-25 22:03:51
+ # @LastEditTime: 2022-04-25 22:27:08
  # @Email: xiangpan@nyu.edu
  # @FilePath: /NYU_DL_Project/make_submissions.sh
  # @Description: 
@@ -13,3 +13,4 @@ mkdir submissions/outputs
 cp ./vicreg/dataset.py ./submissions/
 cp ./vicreg/evaluate.py ./submissions/
 cp ./vicreg/engine.py ./submissions/
+cp ./vicreg/run_evaluate.py ./submissions/
